a8e8920179b87b490d7263f42e3b1467ff8a6db0
Pengyao Zhao <Pengyao.Zhao@smu.ca>
2025-04-09 20:57:27 -0300

    add requirement

--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)nv03(@ui^b325nonbav^gxqx3kkd1ege$5&=99c(zu3w^3%x@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1', '13.58.53.22']
 
 
 # Application definition
